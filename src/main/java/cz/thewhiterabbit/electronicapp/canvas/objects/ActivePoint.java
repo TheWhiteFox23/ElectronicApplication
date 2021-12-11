02618f336751cf8406eb45cc44834fc7c2e34750
theWhiteFox23 <SinglePixelCircle@gmail.com>
2021-12-11 14:32:07 +0100

    Line drawig logic refactored

--- src/main/java/cz/thewhiterabbit/electronicapp/canvas/objects/ActivePoint.java
+++ src/main/java/cz/thewhiterabbit/electronicapp/canvas/objects/ActivePoint.java
@@ -61,96 +61,133 @@ public class ActivePoint extends CanvasObject {
     protected void onObjectDragged(Event e) {
         e.consume();
         if (getParentModel() != null && getParentModel() instanceof GridModel) {
-            CanvasMouseEvent event = (CanvasMouseEvent) e;
-            GridModel m = (GridModel) getParentModel();
-            int gridStartX = getGridX();
-            int gridStartY = getGridY();
-            int gridX = m.getGridCoordinate(event.getX(), m.getOriginX());
-            int gridY = m.getGridCoordinate(event.getY(), m.getOriginY());
-
-            int deltaX = gridX - gridStartX;
-            int deltaY = gridY - gridStartY;
-
-            if (deltaX != 0) {
-                if (firstLine == null) {
-                    firstLine = new TwoPointLineObject(gridStartX, gridStartY, gridX, gridStartY);
-                    //create first line with corresponding orientation
-                } else if (secondLine == null && firstLine.getX2() - firstLine.getX1() == 0) {
-                    secondLine = new TwoPointLineObject(gridStartX, gridY, gridX, gridY);
-                    //create second line with corresponding location
-                } else {
-                    if (firstLine.getX2() - firstLine.getX1() != 0) {
-                        firstLine.setX2(gridX);
-                    } else if (secondLine.getX2() - secondLine.getX1() != 0) {
-                        secondLine.setX2(gridX);
-                        secondLine.setY1(gridY);
-                        secondLine.setY2(gridY);
-                    }
-                }
-            } else if (deltaX == 0) {
-                if (firstLine != null && firstLine.getX2() - firstLine.getX1() != 0) {
-                    firstLine = secondLine;
-                    if(firstLine != null) {
-                        firstLine.setX1(gridStartX);
-                        firstLine.setX2(gridStartX);
-                    }
-                    secondLine = null;
-                } else if (secondLine != null && secondLine.getX2() - secondLine.getX1() != 0) {
-                    secondLine = null;
-                }
-            }
-
-            if (deltaY != 0) {
-                if (firstLine == null) {
-                    firstLine = new TwoPointLineObject(gridStartX, gridStartY, gridStartX, gridY);
-                    //create first line with corresponding orientation
-                } else if (secondLine == null && firstLine.getY2() - firstLine.getY1() == 0) {
-                    secondLine = new TwoPointLineObject(gridX, gridStartY, gridX, gridY);
-                    //create second line with corresponding location
-                } else {
-                    if (firstLine.getY2() - firstLine.getY1() != 0) {
-                        firstLine.setY2(gridY);
-                    } else if (secondLine.getY2() - secondLine.getY1() != 0) {
-                        secondLine.setY2(gridY);
-                        secondLine.setX1(gridX);
-                        secondLine.setX2(gridX);
-                    }
-                }
-            } else if (deltaY == 0) {
-                if (firstLine != null && firstLine.getY2() - firstLine.getY1() != 0) {
-                    firstLine = secondLine;
-                    if(firstLine != null){
-                        firstLine.setY1(gridStartY);
-                        firstLine.setY2(gridStartY);
-                    }
-                    secondLine = null;
-                } else if (secondLine != null && secondLine.getY2() - secondLine.getY1() != 0) {
-                    secondLine = null;
-                }
-            }
-
-            if(firstLine != null || secondLine != null){
-                getEventAggregator().fireEvent(new CanvasPaintEvent(CanvasPaintEvent.REPAINT));
-            }
-
-            if (firstLine != null) {
-                firstLine.setParentModel(m);
-                firstLine.mapProperties();
-                m.updatePaintProperties(firstLine);
-                firstLine.paint(m.getCanvas().getGraphicsContext2D());
-            }
-            if (secondLine != null) {
-                secondLine.setParentModel(m);
-                secondLine.mapProperties();
-                m.updatePaintProperties(secondLine);
-                secondLine.paint(m.getCanvas().getGraphicsContext2D());
-            }
+            onActivePointDragged((CanvasMouseEvent) e);
         }
 
+    }
+
+    private void onActivePointDragged(CanvasMouseEvent e) {
+        CanvasMouseEvent event = e;
+        GridModel m = (GridModel) getParentModel();
+
+        int gridStartX = getGridX();
+        int gridStartY = getGridY();
+        int gridX = m.getGridCoordinate(event.getX(), m.getOriginX());
+        int gridY = m.getGridCoordinate(event.getY(), m.getOriginY());
+        int deltaX = gridX - gridStartX;
+        int deltaY = gridY - gridStartY;
+
+        adjustLines(gridStartX, gridStartY, gridX, gridY, deltaX, deltaY);
+        managePainting(m, deltaX, deltaY);
+    }
+
+    private void adjustLines(int gridStartX, int gridStartY, int gridX, int gridY, int deltaX, int deltaY) {
+        if (deltaX != 0) {
+            setLine(Orientation.HORIZONTAL, gridStartX, gridX, gridStartY, gridY);
+        } else if (deltaX == 0) {
+            deleteLine(Orientation.HORIZONTAL, gridStartX);
+        }
+
+        if (deltaY != 0) {
+            setLine(Orientation.VERTICAL, gridStartY, gridY, gridStartX, gridX);
+        } else if (deltaY == 0) {
+            deleteLine(Orientation.VERTICAL, gridStartY);
+        }
+    }
+
+    private void managePainting(GridModel m, int deltaX, int deltaY) {
+        if((firstLine != null || secondLine != null) || (deltaY == 0 && deltaX == 0)){
+            getEventAggregator().fireEvent(new CanvasPaintEvent(CanvasPaintEvent.REPAINT));
+        }
+
+        if (firstLine != null) {
+            paintLine(m, firstLine);
+        }
+        if (secondLine != null) {
+            paintLine(m, secondLine);
+        }
+    }
+
+    private void paintLine(GridModel m, TwoPointLineObject firstLine) {
+        firstLine.setParentModel(m);
+        firstLine.mapProperties();
+        m.updatePaintProperties(firstLine);
+        firstLine.paint(m.getCanvas().getGraphicsContext2D());
+    }
+
+    private void setLine(Orientation orientation, int firstPoint, int secondPoint, int level1, int level2) {
+        if (firstLine == null) {
+            if(orientation == Orientation.HORIZONTAL){
+                firstLine = new TwoPointLineObject(firstPoint, level1, secondPoint, level1);
+            }else{
+                firstLine = new TwoPointLineObject(level1, firstPoint, level1, secondPoint);
+            }
+        } else if (secondLine == null && getLineOrientation(firstLine) != orientation) {
+            if(orientation == Orientation.HORIZONTAL){
+                secondLine = new TwoPointLineObject(firstPoint, level2, secondPoint, level2);
+            }else{
+                secondLine = new TwoPointLineObject(level2, firstPoint, level2, secondPoint);
+            }
+        } else {
+            adjustLines(orientation, secondPoint, level2);
+        }
+    }
+
+    private void adjustLines(Orientation orientation, int secondPoint, int level2) {
+        if(getLineOrientation(firstLine) == orientation){
+            if(orientation == Orientation.HORIZONTAL){
+                firstLine.setX2(secondPoint);
+            }else{
+                firstLine.setY2(secondPoint);
+            }
+        } else if (getLineOrientation(secondLine) == orientation) {
+            if(orientation == Orientation.HORIZONTAL){
+                secondLine.setX2(secondPoint);
+                secondLine.setY1(level2);
+                secondLine.setY2(level2);
+            }else{
+                secondLine.setY2(secondPoint);
+                secondLine.setX1(level2);
+                secondLine.setX2(level2);
+            }
+
+        }
+    }
+
+    private void deleteLine(Orientation orientation, int newLevel) {
+        if (firstLine != null && getLineOrientation(firstLine) == orientation) {
+            firstLine = secondLine;
+            if(firstLine != null) {
+                if(orientation == Orientation.HORIZONTAL){
+                    firstLine.setX1(newLevel);
+                    firstLine.setX2(newLevel);
+                }else{
+                    firstLine.setY2(newLevel);
+                    firstLine.setY1(newLevel);
+                }
+            }
+            secondLine = null;
+        } else if (secondLine != null && getLineOrientation(secondLine) == orientation) {
+            secondLine = null;
+        }
     }
 
     @Override
     public void clean(GraphicsContext gc) {
         //super.clean(gc);
+    }
+
+    private Orientation getLineOrientation(TwoPointLineObject lineObject){
+            if(lineObject.getY2() == lineObject.getY1()){
+                return Orientation.HORIZONTAL;
+            }else if(lineObject.getX1() == lineObject.getX2()){
+                return Orientation.VERTICAL;
+            }
+            return null;
+    }
+
+    private enum Orientation{
+        HORIZONTAL,
+        VERTICAL
     }
 }
