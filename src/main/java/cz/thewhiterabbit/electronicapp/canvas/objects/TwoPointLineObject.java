a824acd3a255d90ed46f9c56d02f7c7ffac79274
theWhiteFox23 <SinglePixelCircle@gmail.com>
2021-12-15 06:39:29 +0100

    Line object isInBounds (area) implemented

--- src/main/java/cz/thewhiterabbit/electronicapp/canvas/objects/TwoPointLineObject.java
+++ src/main/java/cz/thewhiterabbit/electronicapp/canvas/objects/TwoPointLineObject.java
@@ -5,12 +5,14 @@ import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+import org.apache.commons.math3.linear.*;
 
-public class TwoPointLineObject extends CanvasObject{
+public class TwoPointLineObject extends CanvasObject {
     private IntegerProperty x1;
     private IntegerProperty x2;
     private IntegerProperty y1;
     private IntegerProperty y2;
+
 
     public TwoPointLineObject(int x1, int y1, int x2, int y2) {
         this.x1 = new SimpleIntegerProperty(x1);
@@ -26,39 +28,39 @@ public class TwoPointLineObject extends CanvasObject{
 
     @Override
     protected void doPaint(GraphicsContext gc) {
-        if(getParentModel() != null && getParentModel() instanceof GridModel){
-            GridModel m = (GridModel)getParentModel();
+        if (getParentModel() != null && getParentModel() instanceof GridModel) {
+            GridModel m = (GridModel) getParentModel();
 
             double locationX1 = m.getGridLocation(getX1(), m.getOriginX()) - getLocationX();
             double locationX2 = m.getGridLocation(getX2(), m.getOriginX()) - getLocationX();
             double locationY1 = m.getGridLocation(getY1(), m.getOriginY()) - getLocationY();
             double locationY2 = m.getGridLocation(getY2(), m.getOriginY()) - getLocationY();
 
-            if(isHovered()){
+            if (isHovered()) {
                 gc.setStroke(Color.RED);
-            }else if(isSelected()){
+            } else if (isSelected()) {
                 gc.setStroke(Color.BLUE);
-            }else{
+            } else {
                 gc.setStroke(Color.TURQUOISE);
             }
 
             gc.setLineWidth(getLineWidth());
-            gc.strokeLine(locationX1, locationY1,locationX2, locationY2);
+            gc.strokeLine(locationX1, locationY1, locationX2, locationY2);
         }
     }
 
-    public void mapProperties(){
+    public void mapProperties() {
         this.setGridWidth(Math.abs(getX2() - getX1()));
         this.setGridHeight(Math.abs(getY1() - getY2()));
         this.setGridX((Math.min(getX1(), getX2())));
         this.setGridY((Math.min(getY1(), getY2())));
-        if(getParentModel() != null){
+        if (getParentModel() != null) {
             this.getParentModel().updatePaintProperties(this);
         }
     }
 
-    private void onGridXChanged(){
-        if(getParentModel() != null && getParentModel() instanceof GridModel){
+    private void onGridXChanged() {
+        if (getParentModel() != null && getParentModel() instanceof GridModel) {
             GridModel m = (GridModel) getParentModel();
             int originalGridX = (Math.min(getX1(), getX2()));
             int deltaX = originalGridX - m.getGridCoordinate(getLocationX(), m.getOriginX());
@@ -67,8 +69,8 @@ public class TwoPointLineObject extends CanvasObject{
         }
     }
 
-    private void onGridYChanged(){
-        if(getParentModel() != null && getParentModel() instanceof GridModel){
+    private void onGridYChanged() {
+        if (getParentModel() != null && getParentModel() instanceof GridModel) {
             GridModel m = (GridModel) getParentModel();
             int originalGridY = (Math.min(getY1(), getY2()));
             int deltaY = originalGridY - m.getGridCoordinate(getLocationY(), m.getOriginY());
@@ -78,43 +80,67 @@ public class TwoPointLineObject extends CanvasObject{
     }
 
 
-    public int getX1() {return x1.get();}
+    public int getX1() {
+        return x1.get();
+    }
 
-    public IntegerProperty x1Property() {return x1;}
+    public IntegerProperty x1Property() {
+        return x1;
+    }
 
-    public void setX1(int x1) {this.x1.set(x1);}
+    public void setX1(int x1) {
+        this.x1.set(x1);
+    }
 
-    public int getX2() {return x2.get();}
+    public int getX2() {
+        return x2.get();
+    }
 
-    public IntegerProperty x2Property() {return x2;}
+    public IntegerProperty x2Property() {
+        return x2;
+    }
 
-    public void setX2(int x2) {this.x2.set(x2);}
+    public void setX2(int x2) {
+        this.x2.set(x2);
+    }
 
-    public int getY1() {return y1.get();}
+    public int getY1() {
+        return y1.get();
+    }
 
-    public IntegerProperty y1Property() {return y1;}
+    public IntegerProperty y1Property() {
+        return y1;
+    }
 
-    public void setY1(int y1) {this.y1.set(y1);}
+    public void setY1(int y1) {
+        this.y1.set(y1);
+    }
 
-    public int getY2() {return y2.get();}
+    public int getY2() {
+        return y2.get();
+    }
 
-    public IntegerProperty y2Property() {return y2;}
+    public IntegerProperty y2Property() {
+        return y2;
+    }
 
-    public void setY2(int y2) {this.y2.set(y2);}
+    public void setY2(int y2) {
+        this.y2.set(y2);
+    }
 
-    public int getLoverX(){
+    public int getLoverX() {
         return (Math.min(getX2(), getX1()));
     }
 
-    public int getHigherX(){
+    public int getHigherX() {
         return (Math.max(getX2(), getX1()));
     }
 
-    public int getLoverY(){
+    public int getLoverY() {
         return (Math.min(getY2(), getY1()));
     }
 
-    public int getHigherY(){
+    public int getHigherY() {
         return (Math.max(getY2(), getY1()));
     }
 
@@ -123,22 +149,23 @@ public class TwoPointLineObject extends CanvasObject{
     public boolean isInBounds(double x, double y) {
         GridModel m = (GridModel) getParentModel();
         double tolerance = getLineWidth();
-        if(x<getLocationX()-tolerance || x> getLocationX() + getWidth() + tolerance || y<getLocationY() -tolerance || y>getLocationY() + getHeight() + tolerance)return false;
+        if (x < getLocationX() - tolerance || x > getLocationX() + getWidth() + tolerance || y < getLocationY() - tolerance || y > getLocationY() + getHeight() + tolerance)
+            return false;
         double deltaX = m.getGridLocation(getX2(), m.getOriginX()) - m.getGridLocation(getX1(), m.getOriginX());
         double deltaY = m.getGridLocation(getY2(), m.getOriginY()) - m.getGridLocation(getY1(), m.getOriginY());
 
 
-        if(deltaX == 0 || deltaY == 0){
+        if (deltaX == 0 || deltaY == 0) {
             return true;
-        }else{
-            double a = deltaY/deltaX;
-            double xRelative = x -getLocationX();
-            double yRelative = y -getLocationY();
-            if(a > 0){
-                double yCalculated = a*xRelative;
+        } else {
+            double a = deltaY / deltaX;
+            double xRelative = x - getLocationX();
+            double yRelative = y - getLocationY();
+            if (a > 0) {
+                double yCalculated = a * xRelative;
                 return Math.abs(yRelative - yCalculated) < tolerance;
-            }else{
-                double yCalculated = Math.abs(a)*xRelative;
+            } else {
+                double yCalculated = Math.abs(a) * xRelative;
                 return Math.abs(yRelative - (getHeight() - yCalculated)) < tolerance;
             }
 
@@ -153,34 +180,127 @@ public class TwoPointLineObject extends CanvasObject{
 
     @Override
     public boolean isInBounds(double locationX, double locationY, double width, double height) {
-        if(getParentModel() != null && getParentModel() instanceof  GridModel){
+        if (getParentModel() != null && getParentModel() instanceof GridModel) {
             GridModel m = (GridModel) getParentModel();
 
-            System.out.println("X: " + locationX + " Y: " + locationY + " W: " + width + " H: " + height);
             double x1 = m.getGridLocation(getLoverX(), m.getOriginX());
             double y1 = m.getGridLocation(getLoverY(), m.getOriginY());
             double x2 = m.getGridLocation(getHigherX(), m.getOriginX());
             double y2 = m.getGridLocation(getHigherY(), m.getOriginY());
 
-            //first point in bounds
             boolean firstPointInBounds = (x1 >= locationX && x1 <= locationX + width && y1 >= locationY && y1 <= locationY + height);
-            //second point in bounds
             boolean secondPointInBounds = (x2 >= locationX && x2 <= locationX + width && y2 >= locationY && y2 <= locationY + height);
-            //location x, location y, locationX +height, location y;
 
-            System.out.println("First point in bounds: " + firstPointInBounds);
-            System.out.println("Second point in bounds: " + secondPointInBounds);
+            LineCrate line = new LineCrate(x1,y1, x2, y2);
+            boolean upperXIntersection = isIntersection(line, new LineCrate(locationX, locationY, locationX+width, locationY));
+            boolean lowerXIntersection = isIntersection(line, new LineCrate(locationX, locationY+height, locationX+width, locationY+height));
+            boolean leftYIntersection = isIntersection(line, new LineCrate(locationX, locationY, locationX, locationY+height));
+            boolean rightYIntersection = isIntersection(line, new LineCrate(locationX+width, locationY, locationX+width, locationY+height));
+
+            boolean result = firstPointInBounds || secondPointInBounds || upperXIntersection || lowerXIntersection || leftYIntersection || rightYIntersection;
+            System.out.println(result);
+            return result;
         }
         return false;
     }
 
-    private double getLineWidth(){
+    private double getLineWidth() {
         return 2;
     }
 
     @Override
     public void clean(GraphicsContext gc) {
         super.clean(gc);
+    }
+
+
+    private boolean isIntersection(LineCrate line1, LineCrate line2) {
+        try{
+            line1.tryCalculate();
+            line2.tryCalculate();
+        }catch (Exception e){
+            if((line1.isHorizontal() || line1.isVertical()) && (line2.isVertical() || line1.isHorizontal())){
+                return isIntersectionExtreme(line1, line2);
+            }else{
+                return isIntersection(
+                        new LineCrate(line1.y1, line1.x1, line1.y2, line1.x2),
+                        new LineCrate(line2.y1, line2.x1, line2.y2, line2.x2)
+                );
+            }
+        }
+
+        //TODO:  extract method
+        RealMatrix coefficients =
+                new Array2DRowRealMatrix(new double[][]{{line1.a, -1}, {line2.a, -1}},
+                        false);
+        DecompositionSolver solver = new LUDecomposition(coefficients).getSolver();
+        RealVector constants = new ArrayRealVector(new double[]{-line1.b, -line2.b}, false);
+        RealVector solution;
+
+        try {
+            solution= solver.solve(constants);
+        }catch (Exception e){
+            return false;
+        }
+
+        double intersectionX = solution.getEntry(0);
+        double intersectionY = solution.getEntry(1);
+        return (intersectionX >= Math.min(line1.x1, line1.x2) && intersectionX <= Math.max(line1.x2, line1.x1) &&
+                intersectionX >= Math.min(line2.x1, line2.x2) && intersectionX <= Math.max(line2.x2, line2.x1) &&
+                intersectionY >= Math.min(line1.y1, line1.y2) && intersectionY <= Math.max(line1.y2, line1.y1) &&
+                intersectionY >= Math.min(line2.y1, line2.y2) && intersectionY <= Math.max(line2.y2, line2.y1));
+    }
+    
+    private boolean isIntersectionExtreme(LineCrate line1, LineCrate line2){
+        if(line1.isHorizontal() && line2.isHorizontal()){
+            return line1.y1 == line2.y1;
+        }else if(line1.isHorizontal() && line2.isVertical()){
+            return isIntersectingPerpendicular(line1, line2);
+        }else if(line1.isVertical() && line1.isVertical()){
+            return line1.x1 == line2.x1;
+        }else if(line1.isVertical() && line2.isHorizontal()){
+            return isIntersectingPerpendicular(line2, line1);
+        }else {
+            return false;
+        }
+    }
+
+    private boolean isIntersectingPerpendicular(LineCrate line1, LineCrate line2) {
+        return (line1.y1 >= Math.min(line2.y1, line2.y2) && line1.y1 <= Math.max(line2.y1, line2.y2)
+                && line2.x1 >= Math.min(line1.x1, line1.x2) && line2.x1 <= Math.max(line1.x1, line1.x2));
+    }
+
+    private class LineCrate {
+        double x1, y1, x2, y2, a, b;
+
+        public LineCrate(double x1, double y1, double x2, double y2) {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+        }
+
+        public void tryCalculate() {
+
+            RealMatrix coefficients =
+                    new Array2DRowRealMatrix(new double[][]{{x1, 1}, {x2, 1}},
+                            false);
+            DecompositionSolver solver = new LUDecomposition(coefficients).getSolver();
+            RealVector constants = new ArrayRealVector(new double[]{y1, y2}, false);
+            RealVector solution = solver.solve(constants);
+
+            this.a = solution.getEntry(0);
+            this.b = solution.getEntry(1);
+
+        }
+        
+        public boolean isHorizontal(){
+            return y1 == y2;
+        }
+        
+        public boolean isVertical(){
+            return x1 == x2;
+        }
     }
 
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module cz.thewhiterabbit.electronicapp {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires commons.math3;
 
     opens cz.thewhiterabbit.electronicapp to javafx.fxml;
     exports cz.thewhiterabbit.electronicapp;
