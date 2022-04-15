72cda2e7277d4abb50584c747b68df1015f2a102
TheWhiteFox23 <psycho.artis@gmail.com>
2022-04-15 18:32:33 +0200

    Component menu design finalized

--- src/main/java/cz/thewhiterabbit/electronicapp/view/components/ComponentButton.java
+++ src/main/java/cz/thewhiterabbit/electronicapp/view/components/ComponentButton.java
@@ -5,8 +5,11 @@ import cz.thewhiterabbit.electronicapp.GUIEventAggregator;
 import cz.thewhiterabbit.electronicapp.model.components.Component;
 import cz.thewhiterabbit.electronicapp.view.events.MenuEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Node;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.ClipboardContent;
@@ -18,7 +21,8 @@ import javafx.scene.layout.VBox;
 
 public class ComponentButton extends AnchorPane {
     private Label image = new Label("IMAGE"); //TODO replace with image
-    private Label text;
+    //private Label text;
+    private Tooltip tooltip;
     private Button info;
     private Component component;
     private Image icon;
@@ -29,6 +33,13 @@ public class ComponentButton extends AnchorPane {
         initInfoButton();
         //arrange components
         VBox holder = getHolder(text, component);
+
+        holder.addEventHandler(MouseEvent.MOUSE_MOVED, e->{
+            showTooltip(e);
+        });
+        holder.addEventHandler(MouseEvent.MOUSE_EXITED, e->{
+            hideTooltip();
+        });
 
         getChildren().addAll(holder, info);
         initDragDetection(this);
@@ -48,7 +59,8 @@ public class ComponentButton extends AnchorPane {
         VBox holder = new VBox();
         this.component = component;
         getStylesheets().add(App.class.getResource("stylesheets/style.css").toExternalForm());
-        this.text = new Label(text);
+        //this.text = new Label(text);//TODO text to tooltip
+        tooltip = new Tooltip(text);
         if(component.isImage()){
             icon = new Image(App.class.getResourceAsStream(component.getImagePath()));
             ImageView imageView = new ImageView(icon);
@@ -57,9 +69,9 @@ public class ComponentButton extends AnchorPane {
             double width = icon.getWidth()*co;
             imageView.setFitWidth(width);
             imageView.setFitHeight(height);
-            holder.getChildren().addAll(imageView, this.text);
+            holder.getChildren().addAll(imageView);
         }else{
-            holder.getChildren().addAll(image, this.text);
+            holder.getChildren().addAll(image);
         }
         return holder;
     }
@@ -86,6 +98,15 @@ public class ComponentButton extends AnchorPane {
                 event.consume();
             }
         });
+    }
+
+    private void hideTooltip() {
+        tooltip.hide();
+    }
+
+    private void showTooltip(MouseEvent event) {
+        Node n = (Node) event.getSource();
+        tooltip.show(n, event.getScreenX() + 10, event.getScreenY() + 10);
     }
 
 }
